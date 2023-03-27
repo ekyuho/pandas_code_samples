e22bb1eb4eccfffb8b63be1ecc99b58c86021644
Kyuho Kim <ekyuho@gmail.com>
2023-03-27 23:13:44 +0900

    Update read_weather_rss_send_thingspeak.py

--- read_weather_rss_send_thingspeak.py
+++ read_weather_rss_send_thingspeak.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import time
 from bs4 import BeautifulSoup
 
-r=requests.get('https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1141058500')
-soup = BeautifulSoup(r.content, "html.parser")
-print(soup.temp.string)
+while True:
+  r=requests.get('https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1141058500')
+  soup = BeautifulSoup(r.content, "html.parser")
+  print(soup.temp.string)
 
-r=requests.get(f'https://api.thingspeak.com/update?api_key=L60RYNOQ6CARVMRE&field1={soup.temp.string}')
-print(r.text)
-time.sleep(60*10)  # 60초 * 10분
+  r=requests.get(f'https://api.thingspeak.com/update?api_key=L60RYNOQ6CARVMRE&field1={soup.temp.string}')
+  print(r.text)
+  time.sleep(60*10)  # 60초 * 10분
