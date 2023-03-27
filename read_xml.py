177acb1fa108b0a7f6cba6a1112d4ee2913040e9
Kyuho Kim <ekyuho@gmail.com>
2023-03-27 22:47:29 +0900

    Create read_xml.py

---
+++ read_xml.py
@@ -0,0 +1,14 @@
+# https://freeharmony.tistory.com/64
+import requests
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+
+r=requests.get('https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1141058500')
+soup = BeautifulSoup(r.content, "html.parser")
+data=[]
+for temp in soup.findAll('temp'):
+  print(temp, temp.string)
+  data.append(float(temp.string))
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot(data)
