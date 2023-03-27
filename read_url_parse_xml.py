86439fd892be5890236a11d7341a55e840ee3e4c
Kyuho Kim <ekyuho@gmail.com>
2023-03-27 23:53:28 +0900

    Update read_url_parse_xml.py

--- read_url_parse_xml.py
+++ read_url_parse_xml.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 
 r=requests.get('https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1141058500')
-soup = BeautifulSoup(r.content, "html.parser")
+#soup = BeautifulSoup(r.content, "html.parser")
+soup = BeautifulSoup(r.content, features="xml")   #  pip3 install lxml
 data=[]
 for temp in soup.findAll('temp'):
   print(temp, temp.string)
